c9a852df8f4faea48490eda8071a5a5c63271ba6
zdenek-nemec <zdenek.nemec@artin.cz>
2020-01-14 14:54:07 +0100

    Code cleanup

--- sum.py
+++ sum.py
@@ -5,7 +5,7 @@ while True:
         if int(number) > 0:
             the_sum = 0
             for i in range(1, int(number)+1):
-                the_sum += i;
+                the_sum += i
             print("Da sum is %d, mon." % the_sum)
             break
         else:
